e54f242ec0656d5244598beca1c7038013aa83fd
kiborebrian <104349338+kiborebrian@users.noreply.github.com>
2023-11-16 20:54:52 +0300

    Add files via upload
    this is a revamp of Q1(i)

---
+++ Q1i.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare variables to store input numbers
+    int num1, num2, num3;
+
+    // Get input from the user
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    // Initialize the smallest variable with the first number
+    int smallest = num1;
+
+    // Use switch case to compare and update the smallest variable
+    switch (1) {
+        case 1:
+            if (num2 < smallest) {
+                smallest = num2;
+            }
+            
+        case 2:
+            if (num3 < smallest) {
+                smallest = num3;
+            }
+            
+        default:
+            break;
+    }
+
+    // Output the smallest number
+    cout << "The smallest number is: " << smallest << endl;
+
+    return 0;
+}
